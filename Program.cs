1da81b337a9755770955555a98428928cc086c67
Ira <ira350520@gmail.com>
2025-03-18 15:19:35 +0200

    completed homework

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the first number:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the second number:");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter operator (+, -, *, /): ");
+        char operation = Convert.ToChar(Console.ReadLine());
+
+        int result;
+        switch (operation)
+        {
+            case '+':
+                result = num1 + num2;
+                Console.WriteLine($"Result: {result}");
+                break;
+            case '-':
+                result = num1 - num2;
+                Console.WriteLine($"Result: {result}");
+                break;
+            case '*':
+                result = num1 * num2;
+                Console.WriteLine($"Result: {result}");
+                break;
+            case '/':
+                if (num2 != 0)
+                {
+                    double divResult = (double)num1 / num2;
+                    Console.WriteLine($"Result: {divResult}");
+                }
+                else
+                {
+                    Console.WriteLine("Error: division by zero!");
+                }
+                break;
+            default:
+                Console.WriteLine("Error: Invalid operator!");
+                break;
+        }
+    }
+}
